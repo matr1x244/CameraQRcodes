43b6373431bd6a0c661958c6f842f221f5fd00dc
matr1x244 <zio-21@yandex.ru>
2022-12-13 21:00:50 +0300

    TEXTVIEW GENERATE QR CODES

---
+++ app/src/main/java/com/example/cameraqrcodes/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.cameraqrcodes
+
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatImageView
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.journeyapps.barcodescanner.BarcodeEncoder
+
+class MainActivity : AppCompatActivity() {
+
+    var imageQr: AppCompatImageView? = null
+    var buttonGenerate: AppCompatButton? = null
+    var textViewText: AppCompatEditText? = null
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageQr = findViewById(R.id.imageView_QR)
+        textViewText = findViewById(R.id.edit_text_QR)
+        buttonGenerate = findViewById(R.id.button_generate)
+
+        startGenerate()
+    }
+
+    private fun startGenerate() {
+        val barcodeEncode: BarcodeEncoder = BarcodeEncoder()
+        val defaultQR: Bitmap =
+            barcodeEncode.encodeBitmap("DEFAULT", BarcodeFormat.QR_CODE, 1000, 1000)
+        imageQr?.setImageBitmap(defaultQR)
+
+        buttonGenerate?.setOnClickListener {
+            try {
+                val bitmap: Bitmap = barcodeEncode.encodeBitmap(
+                    textViewText?.text.toString(), BarcodeFormat.QR_CODE, 500, 500
+                )
+                imageQr?.setImageBitmap(bitmap)
+            } catch (e: WriterException) {
+            }
+        }
+    }
+
+}
